7e17feea38f531521224d81788b23b396df3035e
Ashiftal <sumailayussif226@gmail.com>
2025-02-01 21:37:43 +0000

    login function added

--- ebook.py
+++ ebook.py
@@ -7,15 +7,37 @@ print("We are here to make your work easy")
 name= input("Enter your name : ")
 
 
+def signUp():
+    print("Sign up to  GEMA")
+    try:
+        name=str(input("Enter your name : "))
+        emial=input("Enter your email : ")
+        password=input("Enter your password : ")
+        contact=int(input("Enter your contact : "))
+    except ValueError:
+        print("Invalid input")
+    except TypeError:
+        print("Enter a valid name")
+    except Exception as e:
+        print("Invalid input")
+    else:
+        print("Login success")
+        ebook()
 
 
-book = open("Agricultural Technology.pdf", 'rb')
-pdfReader = PyPDF2.PdfReader(book)
-Pages = len(pdfReader.pages)
-print(Pages)
-Pages = pdfReader.pages[Pages-1]
-text = Pages.extract_text()
-speaker = pyttsx3.init()
-speaker.say(text)
-speaker.runAndWait()
+def ebook():
+    book = open("Agricultural Technology.pdf", 'rb')
+    pdfReader = PyPDF2.PdfReader(book)
+    Pages = len(pdfReader.pages)
+    print(Pages)
+    Pages = pdfReader.pages[Pages-1]
+    text = Pages.extract_text()
+    speaker = pyttsx3.init()
+    speaker.say(text)
+    speaker.runAndWait()
 
+
+def main():
+    signUp()  
+if __name__=="__main__":
+    main()
